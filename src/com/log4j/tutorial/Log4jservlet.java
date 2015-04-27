dd0432ddb53b404d40bab79cdeae27c102916937
KP <KP@KP-PC.home>
2015-04-27 14:53:28 -0500

    First commit

---
+++ src/com/log4j/tutorial/Log4jservlet.java
@@ -0,0 +1,46 @@
+package com.log4j.tutorial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+@WebServlet("/Log4jservlet")
+public class Log4jservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Logger logger = null;
+
+	public void init() {
+		logger = Logger.getRootLogger();
+		BasicConfigurator.configure();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		logger.setLevel(Level.FATAL);
+		logger.debug("Logging Debug message");
+		logger.error("Logging error message");
+		logger.info("Logging information message");
+		logger.warn("Logging warning message");
+		logger.fatal("Logging Fatal message");
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<head><title>Log4j Tutorial </title>");
+		writer.println("<body>");
+		writer.println("Log4j w/no configuration file");
+		writer.println("Logger Name:" + logger.getName());
+		writer.println("</body>");
+		writer.println("</html>");
+	}
+
+}
